bbf8377ca7f9ac5c42ea8131980eee80c9a5006a
zb <17764563720@163.com>
2020-12-28 15:53:43 +0800

    添加跨域手动添加一个方法修改Access-Control-Allow-Origin

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app(config_class=None):
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers['Access-Control-Allow-Origin'] = '*'
         if request.method == 'OPTIONS':
             response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
             headers = request.headers.get('Access-Control-Request-Headers')
